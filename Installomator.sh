0147fa5b2a61ef9d2e59787f37f370787c267169
mathieu244 <mathieu.st-yves@hotmail.com>
2021-09-17 10:55:01 -0400

    sublime text
    patch sublime text download url

--- Installomator.sh
+++ Installomator.sh
@@ -2484,8 +2484,8 @@ sublimetext)
     # credit: Mischa van der Bent (@mischavdbent)
     name="Sublime Text"
     type="dmg"
-    downloadURL="https://download.sublimetext.com/latest/stable/osx"
-    appNewVersion=$(curl -fs https://www.sublimetext.com/3 | grep 'class="latest"' | cut -d '>' -f 4 | sed -E 's/ (.*[0-9]*)<.*/\1/g')
+    appNewVersion=$(curl -fs https://www.sublimetext.com/3 | grep 'class="latest"' | cut -d '>' -f 4 | sed -E 's/ (.*[0-9]*)<.*/\1/g') &&
+    downloadURL="https://download.sublimetext.com/Sublime Text $appNewVersion.dmg"
     #appNewVersion=$(curl -Is https://download.sublimetext.com/latest/stable/osx | grep "Location:" | sed -n -e 's/^.*Sublime Text //p' | sed 's/.dmg//g' | sed $'s/[^[:print:]\t]//g') # Alternative from @Oh4sh0
     expectedTeamID="Z6D26JE4Y4"
     ;;
